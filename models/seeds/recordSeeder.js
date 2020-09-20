5c16abc78db9367fa8ed0b2ee8c4f427bd63430c
r05323045 <r05323045@ntu.edu.tw>
2020-09-21 00:03:46 +0800

    revised route

--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -1,5 +1,5 @@
 const recordList = require('../../record.json')
-const Record = require('../record.js')
+const Record = require('../../models/record')
 const db = require('../../config/mongoose')
 
 db.once('open', () => {
