714d8788392d3b6433310f609bc4c93e9d93e7a3
r05323045 <r05323045@ntu.edu.tw>
2020-11-09 20:50:05 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
-// app.js
-// require packages used in the project
 const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
+const session = require('express-session')
 const methodOverride = require('method-override')
 
-// 引用路由器
 const routes = require('./routes')
 require('./config/mongoose')
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
-// setting template engine
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
@@ -22,10 +25,8 @@ app.use(methodOverride('_method'))
 
 app.use(routes)
 
-// setting static files
 app.use(express.static('public'))
 
-// start and listen on the Express server
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
 })
