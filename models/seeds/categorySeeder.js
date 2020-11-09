71930bc393280730a1a3f56f836a32c5963ca612
r05323045 <r05323045@ntu.edu.tw>
2020-11-09 22:50:34 +0800

    modify recordSeeder & categorySeeder

--- models/seeds/categorySeeder.js
+++ models/seeds/categorySeeder.js
@@ -1,11 +1,19 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const Category = require('../categoryModel')
 const db = require('../../config/mongoose')
 
 db.once('open', () => {
-  console.log('mongodb connected!')
   const categoryList = ['家居物業', '交通出行', '休閒娛樂', '餐飲食品', '其他']
-  categoryList.forEach(el => {
-    Category.create({ name: el })
-  })
-  console.log('done')
+  return Promise.all(Array.from(
+    { length: categoryList.length },
+    (_, i) => {
+      return Category.create({ name: categoryList[i] })
+    }
+  ))
+    .then(() => {
+      console.log('done.')
+      process.exit()
+    })
 })
--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -1,11 +1,40 @@
+const bcrypt = require('bcryptjs')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const recordList = require('../../recordData.json')
 const Record = require('../recordModel')
+const User = require('../user')
 const db = require('../../config/mongoose')
 
+const SEED_USER = {
+  name: '老爸',
+  email: 'root@example.com',
+  password: '12345678'
+}
+
 db.once('open', () => {
-  console.log('mongodb connected!')
-  recordList.results.forEach(el => {
-    Record.create(el)
-  })
-  console.log('done')
+  bcrypt
+    .genSalt(10)
+    .then(salt => bcrypt.hash(SEED_USER.password, salt))
+    .then(hash => User.create({
+      name: SEED_USER.name,
+      email: SEED_USER.email,
+      password: hash
+    }))
+    .then(user => {
+      const userId = user._id
+      return Promise.all(Array.from(
+        { length: recordList.results.length },
+        (_, i) => {
+          recordList.results[i].userId = userId
+          return Record.create(recordList.results[i])
+        }
+      ))
+    })
+    .then(() => {
+      console.log('done.')
+      process.exit()
+    })
 })
