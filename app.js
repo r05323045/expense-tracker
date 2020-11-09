1feac7e1f3d00a0e670e4435068123e8edd61752
r05323045 <r05323045@ntu.edu.tw>
2020-11-09 21:55:43 +0800

    feat: add message partials

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const session = require('express-session')
 const methodOverride = require('method-override')
+const flash = require('connect-flash')
 
 const routes = require('./routes')
 
@@ -28,6 +29,14 @@ app.use(methodOverride('_method'))
 app.use(express.static('public'))
 
 usePassport(app)
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
+  next()
+})
 
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated()
